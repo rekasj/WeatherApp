2815e531cd98a3aabb60a88250b77c2032ae0601
Jakub Rękas <jakub.rekas@bitapps.fi>
2023-10-04 20:14:27 +0200

    Initial Commit

---
+++ WeatherApp/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WeatherApp
+//
+//  Created by Jakub Rękas on 04/10/2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WeatherApp/WeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherAppApp.swift
+//  WeatherApp
+//
+//  Created by Jakub Rękas on 04/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
