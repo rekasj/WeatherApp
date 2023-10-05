24b6cb1d5477d675a16f41c0fbc949df065c5eed
rekasj <rekas.jakub@o2.pl>
2023-10-05 18:04:57 +0200

    FEAT: TASK-2 ADD: Layout for mainView

---
+++ WeatherApp/Common/Constants.swift
@@ -0,0 +1,19 @@
+//
+//  Constants.swift
+//  WeatherApp
+//
+//  Created by Jakub Rękas on 04/10/2023.
+//
+
+import Foundation
+
+struct Constants {
+    struct Image {
+        static let backgroundImage = "background"
+        static let locationCircle = "location.circle.fill"
+        static let magnifyingglass = "magnifyingglass"
+    }
+    struct Color {
+        static let primaryColor = "primaryColor"
+    }
+}
